6db25741bba42775de7feb84a17dc7632a71873b
lucas <lucas@PC-L>
2022-11-06 23:50:05 -0300

    Agregar archivos de proyecto.

---
+++ Proyecto Lucas-Montenegro/Class/Class.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Lucas_Montenegro.Class
+{
+    public class Usuario
+    {
+        int Id;
+        String Nombre;
+        String Apellido;
+        String NombreUsuario;
+        String Contraseña;
+        String Mail;
+    }
+    public class Producto
+    {
+        int Id;
+        String Descripcion;
+        int Costo;
+        int PrecioVenta;
+        int Stock;
+        int IdUsuario;
+    }
+    public class ProductoVendido
+    {
+        int Id;
+        int IdProducto;
+        int Stock;
+        int IdVenta;
+    }
+    public class Venta
+    {
+        int Id;
+        String Comentarios;
+    }
+}
---
+++ Proyecto Lucas-Montenegro/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("30");
+string PrimeraVariable = "GoldenFreezer";
+Console.WriteLine(PrimeraVariable);
+int VariableNumerica = 50;
+int VariableNumericaDos = 75;
+Console.WriteLine(VariableNumerica + VariableNumericaDos);
+VariableNumerica = 60;
+Console.WriteLine(VariableNumerica);
